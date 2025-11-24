8e4a38f66671185f1373f47f3ec4d675b0184ef0
GeaucefStone <geaucefstone@proton.me>
2025-11-24 14:57:23 -0600

    Update file_loader.js

--- sources/markdown_reader/file_loader.js
+++ sources/markdown_reader/file_loader.js
@@ -15,6 +15,7 @@ const documentRegistry = {
     'poetic_justice': 'contents/education/textbook/00_analysis-flexible_sentencing.md',
     'proxicide_doctrine': 'contents/education/textbook/01A_analysis-proxicide_doctrine.md',
     'privacy_and_surveilance': 'contents/education/textbook/01A0_analysis-balance_of_privacy_and_surveilance.md',
+    'private_prisons': 'contents/education/textbook/01A01_analysis-private_prisons.md', 
     'tiered_self_defense_framework': 'contents/education/textbook/01B_analysis-tiered_self_defense_framework.md',
     'defense_and_transferred_liability': 'contents/education/textbook/01C_analysis-defense_and_transferred_liability.md',
     'the_last_landlord': 'contents/education/textbook/01D_the_last_landlord.md',
